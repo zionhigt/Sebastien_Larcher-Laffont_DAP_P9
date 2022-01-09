13a32cf2982850a6652b581faa987a222761e2c8
zionhigt <zion@SHADOW-BO4F2QG2.localdomain>
2022-01-09 13:41:45 +0100

    [UPDT]

--- P9/LITReview/ticket/views.py
+++ P9/LITReview/ticket/views.py
@@ -9,21 +9,25 @@ class CreateTicketView(View):
     form_class = CreateTicketForm
 
     def get(self, request):
-        form = self.form_class()
-        return render(request, 'ticket/create_page.html', {
-            'form': form
-        })
+        if not request.user.is_anonymous:
+            form = self.form_class()
+            return render(request, 'ticket/create_page.html', {
+                'form': form
+            })
+        return redirect('login')
 
     def post(self, request):
-        form = self.form_class(request.POST, request.FILES)
-        if form.is_valid():
-            without_user = form.save(commit=False)
-            without_user.user = User.objects.get(id=request.user.id)
-            without_user.save()
-            return redirect('home')
-        return render(request, 'ticket/create_page.html', {
-            'form': form
-        })
+        if not request.user.is_anonymous:
+            form = self.form_class(request.POST, request.FILES)
+            if form.is_valid():
+                without_user = form.save(commit=False)
+                without_user.user = User.objects.get(id=request.user.id)
+                without_user.save()
+                return redirect('home')
+            return render(request, 'ticket/create_page.html', {
+                'form': form
+            })
+        return redirect('login')
 
 
 class UpdateTicketView(View):
